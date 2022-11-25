103e717b008b8360553cb5b8b3badf6ae3e18298
Aaron <ztoplin@yahoo.com>
2022-11-25 17:15:47 -0500

    commit tree 6eec42692592d46bd6d756df8cf74203b6fb6ec2

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+
+    def initialize(title_value)
+        @title = title_value
+    end
+
+    def turn_page 
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
 
